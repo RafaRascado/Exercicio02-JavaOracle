fe7bee2191d7e3b91f8926bf87e89810c822e2a7
RafaRascado <129122750+RafaRascado@users.noreply.github.com>
2023-05-27 16:41:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("   xxxx ");
+    System.out.println(" x     x");
+    System.out.print("x  x  x  x");
+    System.out.print("\nx        x\nx x    x x\nx  x x   x\n");
+     System.out.println(" x     x");
+   System.out.println("   xxxx ");
+  }
+}
